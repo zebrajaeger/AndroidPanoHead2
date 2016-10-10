43a552e5200a771fa5c4984a9e0fc32ee3324f78
Lars <L@rs-Brandt.de>
2016-10-10 23:08:21 +0200

    add green and red line to angle view

--- app/src/main/java/de/zebrajaeger/androidpanohead2/AngleView.java
+++ app/src/main/java/de/zebrajaeger/androidpanohead2/AngleView.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -21,13 +24,18 @@ public class AngleView extends View {
   private float centerX;
   private float centerY;
   private float radius;
+  @Nullable
   private Path pointer;
   private Paint scalaPaint;
   private Paint pointerPaint;
   private Bitmap camOriginalImg;
   private Bitmap camIScaledImg;
-  private float angle1 = 10;
-  private float angle2 = 30;
+  @Nullable
+  private Float angle1 = 10f;
+  @Nullable
+  private Float angle2 = 30f;
+  @Nullable
+  private Float angleRed = 45f;
 
   public AngleView(Context context) {
     super(context);
@@ -52,20 +60,20 @@ public class AngleView extends View {
     camOriginalImg = BitmapFactory.decodeResource(getResources(), R.drawable.cam2_0400);
   }
 
-  public void setAngle(float angle1, float angle2) {
+  public void setAngle(@Nullable Float angle1, @Nullable Float angle2) {
     this.angle1 = angle1;
     this.angle2 = angle2;
     updatePointer();
     postInvalidate();
   }
 
-  public void setAngle1(float angle) {
+  public void setAngle1(@Nullable Float angle) {
     this.angle1 = angle;
     updatePointer();
     postInvalidate();
   }
 
-  public void setAngle2(float angle) {
+  public void setAngle2(@Nullable Float angle) {
     this.angle1 = angle;
     updatePointer();
     postInvalidate();
@@ -85,33 +93,83 @@ public class AngleView extends View {
   }
 
   private void updatePointer() {
-    Path path = new Path();
-    float r1 = radius;
+    if (angle1 != null && angle2 != null) {
+      Path path = new Path();
+      float r1 = radius;
 
-    path.moveTo(centerX, centerY);
-    float x1 = (float) (Math.sin( Math.toRadians(angle1)) * r1);
-    x1 += centerX;
-    float y1 = -(float) (Math.cos(Math.toRadians(angle1)) * r1);
-    y1 += centerY;
-    path.lineTo(x1, y1);
+      path.moveTo(centerX, centerY);
+      float x1 = (float) (Math.sin(Math.toRadians(angle1)) * r1);
+      x1 += centerX;
+      float y1 = -(float) (Math.cos(Math.toRadians(angle1)) * r1);
+      y1 += centerY;
+      path.lineTo(x1, y1);
 
-    float x2 = (float) (Math.sin(Math.toRadians(angle2)) * r1);
-    x2 += centerX;
-    float y2 = -(float) (Math.cos(Math.toRadians(angle2)) * r1);
-    y2 += centerY;
-    path.lineTo(x2, y2);
+      float x2 = (float) (Math.sin(Math.toRadians(angle2)) * r1);
+      x2 += centerX;
+      float y2 = -(float) (Math.cos(Math.toRadians(angle2)) * r1);
+      y2 += centerY;
+      path.lineTo(x2, y2);
 
-    path.close();
+      path.close();
 
-    pointer = path;
+      pointer = path;
+    } else {
+      pointer = null;
+    }
   }
 
   @Override
   protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
 
-    canvas.drawPath(pointer, pointerPaint);
+    // draw cam view field if exists
+    if (pointer != null) {
+      canvas.drawPath(pointer, pointerPaint);
+    }
 
+    // draw black line
+    Float angleBlack = null;
+    if (angle1 != null || angle2 != null) {
+      angleBlack = (angle1 + angle2) / 2f;
+    } else if (angle1 != null) {
+      angleBlack = angle1;
+    } else if (angle2 != null) {
+      angleBlack = angle2;
+    }
+
+    if (angleBlack != null) {
+      float x1 = (float) (Math.sin(Math.toRadians(angleBlack)) * radius);
+      x1 += centerX;
+      float y1 = -(float) (Math.cos(Math.toRadians(angleBlack)) * radius);
+      y1 += centerY;
+
+      Paint p = new Paint();
+      p.setColor(Color.GREEN);
+      p.setStyle(Paint.Style.FILL);
+      p.setStrokeWidth(5f);
+      canvas.drawLine(centerX, centerY, x1, y1, p);
+    }
+
+    // draw red line
+    if (angleRed != null) {
+      float x1 = (float) (Math.sin(Math.toRadians(angleRed)) * radius);
+      x1 += centerX;
+      float y1 = -(float) (Math.cos(Math.toRadians(angleRed)) * radius);
+      y1 += centerY;
+
+      Paint p = new Paint();
+      p.setColor(Color.RED);
+      p.setStyle(Paint.Style.STROKE);
+      p.setPathEffect(new DashPathEffect(new float[]{7, 3}, 0));
+      p.setStrokeWidth(2f);
+
+      Path path = new  Path();
+      path.moveTo(centerX, centerY);
+      path.lineTo(x1, y1);
+      canvas.drawPath(path,p);
+    }
+
+    // draw pitch lines
     float r1 = radius * 0.9f;
     float r10 = radius * 0.8f;
     float r90 = radius * 0.7f;
@@ -130,17 +188,18 @@ public class AngleView extends View {
 
       float x1 = (float) (Math.sin(angle) * ri);
       x1 += centerX;
-      float y1 = (float) (Math.cos(angle) * ri);
+      float y1 = -(float) (Math.cos(angle) * ri);
       y1 += centerY;
 
       float x2 = (float) (Math.sin(angle) * radius);
       x2 += centerX;
-      float y2 = (float) (Math.cos(angle) * radius);
+      float y2 = -(float) (Math.cos(angle) * radius);
       y2 += centerY;
       canvas.drawLine(x1, y1, x2, y2, scalaPaint);
       angle += d;
     }
 
+    // draw camera
     Matrix m = new Matrix();
     canvas.save();
     canvas.rotate((angle1 + angle2) / 2.0f, centerX, centerY);
